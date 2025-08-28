5a9d52b1320d4222da82784a210a1314a61f9f69
Ellis Martin <elliswmartin@gmail.com>
2025-08-28 12:02:37 -0700

    Update shhelpersLfA-2025.sh

--- shhelpersLfA-2025.sh
+++ shhelpersLfA-2025.sh
@@ -2,15 +2,17 @@
 
 echo "Greetings $USER 🦋"
 
-### Set to folder containing images to process
-BASE_DIR="/PATH/TO/IMAGES"
+BASE_DIR="/Users/ellis/Downloads/veloz"  ### Set to folder containing images to process
 
 echo "This script allows you to do multiple shell tasks in one! 
-Press 'j' to make jps from tifs in tiff-process folder 
-Press 'c' to autocrop jpgs in jpg-process folder
-Press 'm' to add 40px margin to fullsize jpgs
-Press 'r' to resize jpgs and make mids in cropped folder
-Press 'q' to quit"
+Working directly in: $BASE_DIR
+
+Press 'j' to convert .tif files to .jpg
+Press 'c' to autocrop .jpg files
+Press 'm' to add 40px margin to .jpg files
+Press 'r' to resize .jpg files and make mids
+Press 'q' to quit
+"
 
 while [[ ! $REPLY =~ ^[Qq]$ ]]; do
     read -p "Please make a selection: " -n 1 -r
@@ -18,91 +20,79 @@ while [[ ! $REPLY =~ ^[Qq]$ ]]; do
 
     case $REPLY in
         [Jj])
-            mkdir -p "$BASE_DIR/tiff-process"
-            files="$BASE_DIR/tiff-process/*.tif"
+            files="$BASE_DIR"/*.tif
             total_files=$(ls -1 $files 2>/dev/null | wc -l)
             current_file=0 
 
             for file in $files; do
-                printf "\r🔁🔁 Converting %d of %d TIFFs to JPGs\033[K" "$((++current_file))" "$total_files"
+                printf "\r🔁 Converting %d of %d TIFFs to JPGs\033[K" "$((++current_file))" "$total_files"
                 convert "$file" -flatten "${file%.tif}.jpg" 2>/dev/null
-                [[ $? -ne 0 ]] && printf "\n❗❗Error converting $file\n"
+                [[ $? -ne 0 ]] && printf "\n❗ Error converting $file\n"
             done
-
-            if [[ -n $(ls "$BASE_DIR/tiff-process"/*.jpg 2>/dev/null) ]]; then
-                mkdir -p "$BASE_DIR/jpg-process" && mv "$BASE_DIR/tiff-process"/*.jpg "$BASE_DIR/jpg-process/"
-                echo    
-                echo ⭐ Jpgs created, see jpg-process folder.
-            else
-                echo ❌ No JPGs were created.
-            fi
+            echo -e "\n✅ TIFF to JPG conversion complete."
             ;;
 
         [Cc])
-            for file in "$BASE_DIR/jpg-process"/*; do
-                mv "$file" "${file/.jpeg/.jpg}" 2>/dev/null
+            # Normalize file extensions
+            for file in "$BASE_DIR"/*.jpeg; do
+                mv "$file" "${file%.jpeg}.jpg" 2>/dev/null
             done
-            echo 🦩 Extensions renamed to .jpg.
-            mkdir -p "$BASE_DIR/cropped" && cp -R "$BASE_DIR/jpg-process"/*.jpg "$BASE_DIR/cropped/"
-            echo 📁 Files copied and moved to cropped folder. 
-            echo 🪨🔨 Now on to cropping! hold please ☺ 
-            
-            files="$BASE_DIR/cropped/*.jpg"
+            echo 🦩 Extensions renamed to .jpg if needed.
+
+            files="$BASE_DIR"/*.jpg
             total_files=$(ls -1 $files 2>/dev/null | wc -l)
             current_file=0
 
             for file in $files; do
-                printf "\r🔁🔁 Cropping %d of %d jpgs\033[K" "$((++current_file))" "$total_files"
+                printf "\r🔁 Cropping %d of %d JPGs\033[K" "$((++current_file))" "$total_files"
                 mogrify -bordercolor white -fuzz 3% -trim +repage -border 8x8 "$file" 2>/dev/null
-                [[ $? -ne 0 ]] && printf "\n❗❗Error cropping $file\n"
+                [[ $? -ne 0 ]] && printf "\n❗ Error cropping $file\n"
             done 
-            echo
-            echo 🌻 Cropping complete! 
-            echo
+            echo -e "\n🌻 JPG cropping complete."
             ;;
 
         [Mm])
-            mkdir -p "$BASE_DIR/cropped-margin" && cp -R "$BASE_DIR/cropped"/*.jpg "$BASE_DIR/cropped-margin/"
-            echo 🪟🔖 Now to add 40px margin, hold please 🌅 
-
-            files="$BASE_DIR/cropped-margin/*.jpg"
+            files="$BASE_DIR"/*.jpg
             total_files=$(ls -1 $files 2>/dev/null | wc -l)
             current_file=0
 
             for file in $files; do
-                printf "\r🔁🔁 Adjusting %d of %d jpgs\033[K" "$((++current_file))" "$total_files"
+                printf "\r🔁 Adding margin to %d of %d JPGs\033[K" "$((++current_file))" "$total_files"
                 convert "$file" -bordercolor white -border 40x40 "$file" 2>/dev/null
-                [[ $? -ne 0 ]] && printf "\n❗❗Error adjusting margins on $file\n"
+                [[ $? -ne 0 ]] && printf "\n❗ Error adding margin to $file\n"
             done        
-            echo -e "\n🌲 Images now include 40px margin. See cropped-margin folder for files\n"  
+            echo -e "\n🌲 Margin added to JPGs."
             ;;
 
         [Rr])
-            mkdir -p "$BASE_DIR/oa" && cp -R "$BASE_DIR/cropped"/*.jpg "$BASE_DIR/oa/"
-            echo 🪚🪵 Now on to downsizing, hold please 🐗
-
-            files="$BASE_DIR/oa/*.jpg"
+            files="$BASE_DIR"/*.jpg
             total_files=$(ls -1 $files 2>/dev/null | wc -l)
             current_file=0
 
             for file in $files; do
-                printf "\r🔁🔁 Resizing %d of %d jpgs\033[K" "$((++current_file))" "$total_files"
+                printf "\r🔁 Resizing %d of %d JPGs\033[K" "$((++current_file))" "$total_files"
+
+                # Create a mid-size version (800px max) next to original
+                cp -n "$file" "${file%.*}_mid.jpg"
+                mogrify -resize 800x800\> "${file%.*}_mid.jpg" 2>/dev/null
+
+                # Resize original to max 3000px
                 mogrify -resize 3000x3000\> "$file" 2>/dev/null
-                [[ $? -ne 0 ]] && printf "\n❗❗Error resizing $file\n"
+
+                [[ $? -ne 0 ]] && printf "\n❗ Error resizing $file\n"
             done        
-            echo
-            echo 🌲 All images resized at 3000px. See oa folder for files 
-            echo 
+            echo -e "\n🌲 Resizing complete: 3000px fulls & 800px mids created."
             ;;
+
         [Qq])
-            echo 🦩 Quitting now 
+            echo 🦩 Quitting now.
             ;;
         *)
-            echo -e "Invalid selection. 
-            \nPress 'j' to make jps from tifs in tiff-process folder 
-            \nPress 'c' to autocrop jps in jpg-process folder
-            \nPress 'm' to add 40px margin to fullsize jpgs
-            \nPress 'r' to resize jpgs
+            echo -e "Invalid selection.
+            \nPress 'j' to convert .tif to .jpg
+            \nPress 'c' to autocrop .jpg files
+            \nPress 'm' to add 40px margin to .jpg files
+            \nPress 'r' to resize .jpg files and create mids
             \nPress 'q' to quit"
             ;;
     esac
